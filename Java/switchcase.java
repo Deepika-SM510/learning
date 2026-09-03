904a69886018d7d88e32938405dd3980d1ea9c90
Deepika <deepikasm755@gmail.com>
2026-09-03 15:35:17 +0530

    Added a switch case program

---
+++ Java/switchcase.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class switchcase{
+    public static void main (String[] args){
+        Scanner input =new Scanner(System.in);
+        String fruits=input.next();
+        switch(fruits){
+            case "Mango":
+                System.out.println("King of fruits!");
+                break;
+            case "Apple":
+                System.out.println("Healthiest fruit!");
+                break;
+            case "Orange":
+                System.out.println("Vitamin C powerhouse!");
+                break;
+            case "Banana":
+                System.out.println("Great for potassium!");
+                break;
+            case "Grapes":
+                System.out.println("sweetest fruit!");
+                break;
+            case "Pineapple":
+                System.out.println("Not good at summer");
+                break;
+            default:
+                System.out.println("enter proper fruit name!");
+        
+        }
+
+}  
+
+
+}
